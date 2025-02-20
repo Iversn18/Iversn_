87b09718a82cb55bb1fb0c0715e2dffde90ef156
iversn <itdeguzman@paterostechnologicalcollege.ph>
2025-02-20 11:37:48 +0800

    final copy of prog1

--- prog1.java
+++ prog1.java
@@ -1,6 +1,8 @@
 publick class prog 1
    public static void main (String[]  args){
-     System.out.printIn(" PATEROS TECHNOLOGICAL COLLEGE");
-         
+     System.out.println(" PATEROS TECHNOLOGICAL COLLEGE");
+     System.out.println(" IVERSON DE GUZMAN");
+     System.out.println(" BSIT-2L");
+     System.out.println(" WHITE");
    }
 }
